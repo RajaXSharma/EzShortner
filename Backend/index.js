1761bcbce408d5f5586659e1be7be218a0bc1735
RajaXSharma <rajasharma844891@gmail.com>
2024-08-23 17:59:41 +0530

    completed backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import DBCONN from "./DBCONN.js";
+import { Url } from "./models/url.model.js";
+import multer from "multer";
+
 const app = express();
 dotenv.config();
+app.use(express.json());
+const upload = multer();
 
 DBCONN()
   .then(() => {
@@ -14,6 +19,27 @@ DBCONN()
     console.log("error at db conn on indexjs", err);
   });
 
-  
+app.post("/", upload.none(), async (req, res) => {
+  const { originalUrl } = req.body;
 
+  const exist = await Url.findOne({ oriUrl: originalUrl });
 
+  if (exist) {
+    res.json({ shortUrl: exist.shortUrl });
+  } else {
+    const shortUrl = await Url.create({ oriUrl: originalUrl });
+    res.json({ shortUrl: shortUrl.shortUrl });
+  }
+});
+
+app.get("/:shortedUrl", async (req, res) => {
+  const shortedUrl = await Url.findOne({ shortUrl: req.params.shortedUrl });
+
+  if (!shortedUrl) {
+    throw new Error("cant find url");
+    
+  }
+  // res.json(req.params)
+  // res.json(shortedUrl)
+  res.redirect(shortedUrl.oriUrl);
+});
--- Backend/models/url.model.js
+++ Backend/models/url.model.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
+import shortId from "shortid";
 
 const urlSchema = mongoose.Schema({
-    oriUrl: {
-        type: String,
-        required: true
-    },
-    shortUrl: {
-        t
-    }
-})
+  oriUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    default: shortId.generate,
+  },
+});
+
+export const Url = mongoose.model("Url", urlSchema);
