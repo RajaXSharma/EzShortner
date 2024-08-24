960a14bff311e30eb4b07ce75cd696d0d99477a5
RajaXSharma <rajasharma844891@gmail.com>
2024-08-24 18:09:45 +0530

    cors fixes

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import DBCONN from "./DBCONN.js";
 import { Url } from "./models/url.model.js";
 import multer from "multer";
-import cors from "cors";
+import cors from "cors"
 
 const app = express();
 dotenv.config();
@@ -28,7 +28,9 @@ app.post("/", upload.none(), async (req, res) => {
   const exist = await Url.findOne({ oriUrl: originalUrl });
 
   if (exist) {
-    res.json({ shortUrl: `localhost:4000/${exist.shortUrl}` });
+    res.json({
+      shortUrl: `https://ez-shortner-api.vercel.app/${exist.shortUrl}`,
+    });
   } else {
     const shortUrl = await Url.create({ oriUrl: originalUrl });
     res.json({ shortUrl: `localhost:4000/${shortUrl.shortUrl}` });
@@ -40,8 +42,8 @@ app.get("/:shortedUrl", async (req, res) => {
 
   if (!shortedUrl) {
     res.status(404).json({
-      "error":"link not found"
-    })
+      error: "link not found",
+    });
   }
   // res.json(req.params)
   // res.json(shortedUrl)
@@ -51,5 +53,3 @@ app.get("/:shortedUrl", async (req, res) => {
 app.use((req, res, next) => {
   res.status(404).send("404 - Page Not Found");
 });
-
-
